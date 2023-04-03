6f5a4ed8f0adcc9bd4f04312ddf403a8528af9c6
Malcottan <maxtryasiy@gmail.com>
2023-04-03 10:41:14 +0300

    NumCheck

---
+++ Project3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Num; 
+    cin >> Num;
+    if (Num % 2 == 0){
+        cout << "+";
+    }
+    else {
+        cout << "-";
+    }
+}
